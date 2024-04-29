f27a6c84c97e4a1f8137f8b2ef211256965ba9cb
Conquepo <angel.salo467@gmail.com>
2024-04-30 01:59:39 +0200

    Nuevo diseño sin acabar de admin

--- Practica final PVA/FormularioAdmin.cs
+++ Practica final PVA/FormularioAdmin.cs
@@ -16,12 +16,5 @@ namespace Practica_final_PVA
         {
             InitializeComponent();
         }
-
-        private void linklblProductosExistentes_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            ProductosExistentes productosExistentes = new ProductosExistentes();
-            productosExistentes.Show();
-            this.Hide();
-        }
     }
 }
