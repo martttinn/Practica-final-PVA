e59a969ed80e12dc6cf5f4ca01a57b13509ee0c7
Martín lucas <martinlucas2003@gmail.com>
2024-04-29 11:54:13 +0200

    Renovacion de interfaz de login y añadido de logo como icono

--- Practica final PVA/Form1.Designer.cs
+++ Practica final PVA/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.txtDni = new System.Windows.Forms.TextBox();
             this.txtContrasena = new System.Windows.Forms.TextBox();
@@ -43,6 +44,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell Condensed", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(234)))));
             this.label1.Location = new System.Drawing.Point(116, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(320, 30);
@@ -70,7 +72,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(173)))), ((int)(((byte)(45)))));
             this.label2.Location = new System.Drawing.Point(117, 81);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 20);
@@ -81,7 +83,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(173)))), ((int)(((byte)(45)))));
             this.label3.Location = new System.Drawing.Point(53, 119);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(105, 20);
@@ -102,10 +104,12 @@
             // rbAdmin
             // 
             this.rbAdmin.AutoSize = true;
+            this.rbAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAdmin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(96)))), ((int)(((byte)(19)))));
             this.rbAdmin.Location = new System.Drawing.Point(197, 193);
             this.rbAdmin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rbAdmin.Name = "rbAdmin";
-            this.rbAdmin.Size = new System.Drawing.Size(66, 20);
+            this.rbAdmin.Size = new System.Drawing.Size(71, 20);
             this.rbAdmin.TabIndex = 6;
             this.rbAdmin.TabStop = true;
             this.rbAdmin.Text = "Admin";
@@ -114,10 +118,12 @@
             // rbUsuario
             // 
             this.rbUsuario.AutoSize = true;
+            this.rbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(96)))), ((int)(((byte)(19)))));
             this.rbUsuario.Location = new System.Drawing.Point(277, 193);
             this.rbUsuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rbUsuario.Name = "rbUsuario";
-            this.rbUsuario.Size = new System.Drawing.Size(75, 20);
+            this.rbUsuario.Size = new System.Drawing.Size(82, 20);
             this.rbUsuario.TabIndex = 7;
             this.rbUsuario.TabStop = true;
             this.rbUsuario.Text = "Usuario";
@@ -126,10 +132,12 @@
             // cbMostrar
             // 
             this.cbMostrar.AutoSize = true;
+            this.cbMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMostrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(96)))), ((int)(((byte)(19)))));
             this.cbMostrar.Location = new System.Drawing.Point(188, 146);
             this.cbMostrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbMostrar.Name = "cbMostrar";
-            this.cbMostrar.Size = new System.Drawing.Size(144, 20);
+            this.cbMostrar.Size = new System.Drawing.Size(162, 20);
             this.cbMostrar.TabIndex = 8;
             this.cbMostrar.Text = "Mostrar contraseña";
             this.cbMostrar.UseVisualStyleBackColor = true;
@@ -139,8 +147,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(536, 297);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(30)))), ((int)(((byte)(11)))));
+            this.ClientSize = new System.Drawing.Size(537, 294);
             this.Controls.Add(this.cbMostrar);
             this.Controls.Add(this.rbUsuario);
             this.Controls.Add(this.rbAdmin);
@@ -150,9 +158,10 @@
             this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.txtDni);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "Inicio de sesión";
+            this.Text = "Sandwich Sprint - Iniciar sesión";
             this.ResumeLayout(false);
             this.PerformLayout();
 
