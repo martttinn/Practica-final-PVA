8c3b8c7dba468c75233449878a601b90d819223e
Martín lucas <martinlucas2003@gmail.com>
2024-05-03 12:28:37 +0200

    flechitas

--- Practica final PVA/panelInicio.Designer.cs
+++ Practica final PVA/panelInicio.Designer.cs
@@ -313,7 +313,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(374, 665);
+            this.button1.Location = new System.Drawing.Point(373, 665);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(148, 33);
             this.button1.TabIndex = 34;
